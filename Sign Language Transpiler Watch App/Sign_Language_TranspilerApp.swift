72c79504fd574c99d3d81ddb5117117c5b466dda
Nikola Andreev <andr.nikola.08@gmail.com>
2025-08-20 15:26:42 +0100

    Initial Commit

---
+++ Sign Language Transpiler Watch App/Sign_Language_TranspilerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sign_Language_TranspilerApp.swift
+//  Sign Language Transpiler Watch App
+//
+//  Created by Nikola Andreev on 20.08.25.
+//
+
+import SwiftUI
+
+@main
+struct Sign_Language_Transpiler_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Sign Language Transpiler/Sign_Language_TranspilerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sign_Language_TranspilerApp.swift
+//  Sign Language Transpiler
+//
+//  Created by Nikola Andreev on 20.08.25.
+//
+
+import SwiftUI
+
+@main
+struct Sign_Language_TranspilerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
